2752bd4629bc7172fe2a52fac9d9b614edfcf37f
ThiagoNY <thiagonishiyama10@outlook.com>
2026-03-08 19:51:51 -0300

    Add files via upload

---
+++ consultasTotais.sql
@@ -0,0 +1,43 @@
+-- Tabela: advisor
+SELECT COUNT(*) AS total FROM advisor;
+-- Quantidade de registros: [2000]
+
+-- Tabela: classroom
+SELECT COUNT(*) AS total FROM classroom;
+-- Quantidade de registros: [30]
+
+-- Tabela: course
+SELECT COUNT(*) AS total FROM course;
+-- Quantidade de registros: [200]
+
+-- Tabela: department
+SELECT COUNT(*) AS total FROM department;
+-- Quantidade de registros: [20]
+
+-- Tabela: instructor
+SELECT COUNT(*) AS total FROM instructor;
+-- Quantidade de registros: [50]
+
+-- Tabela: prereq
+SELECT COUNT(*) AS total FROM prereq;
+-- Quantidade de registros: [100]
+
+-- Tabela: section
+SELECT COUNT(*) AS total FROM section;
+-- Quantidade de registros: [100]
+
+-- Tabela: student
+SELECT COUNT(*) AS total FROM student;
+-- Quantidade de registros: [2000]
+
+-- Tabela: takes
+SELECT COUNT(*) AS total FROM takes;
+-- Quantidade de registros: [3000]
+
+-- Tabela: teaches
+SELECT COUNT(*) AS total FROM teaches;
+-- Quantidade de registros: [100]
+
+-- Tabela: time_slot
+SELECT COUNT(*) AS total FROM time_slot;
+-- Quantidade de registros: [20]
